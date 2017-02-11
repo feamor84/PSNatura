5b68c5059ea65b0c6aac7f6c427698d058855803
bartek <mail@bartekpawlowski.pl>
2017-02-11 18:36:26 +0100

    Add aspect ratio to MainImageView in MainActivity.java and set it to 3/2

--- app/src/main/java/pl/bartekpawlowski/psnatura/MainActivity.java
+++ app/src/main/java/pl/bartekpawlowski/psnatura/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
         // SET ADDRESS TEXTVIEW CLICKABLE
         TextView address = (TextView) findViewById(R.id.Address);
         address.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // SET ASPECT RATIO TO MainImageView
+        DisplayMetrics dimension = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dimension);
+        int screenWidth = dimension.widthPixels;
+
+        float ascpectRatio = (float) 3 / (float) 2;
+        float imageWidth = screenWidth / ascpectRatio;
+        ImageView mainImage = (ImageView) findViewById((R.id.MainImageView));
+        mainImage.getLayoutParams().height = (int) imageWidth;
+
     }
 }
